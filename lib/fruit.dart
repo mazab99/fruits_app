4826889c3672dbc2930e16c91370ea9dbce270bb
mazab99 <mazab15499@gmail.com>
2023-03-20 09:26:19 +0200

    finish

---
+++ lib/fruit.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+import 'package:fruits_app/main.dart';
+
+class Fruit{
+  String name ;
+  String image ;
+  double price ;
+  int qty =0;
+  Color color;
+  Position? position;
+  bool animate =false;
+
+  Fruit(this.name, this.image, this.price, this.qty,this.color);
+
+  set Qty(int qty)=>this.qty=qty;
+  set reAnimate(bool animate)=>this.animate =animate;
+
+  set currentPosition(Position? position)=>this.position=position;
+
+
+
+}
